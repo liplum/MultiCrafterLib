ce3d6bfa8bbee7c7d4c2a2ac97b70e5dc311ffd5
Liplum <Li_plum@outlook.com>
2023-12-18 15:22:51 +0800

    full java example

--- java/src/WithJsonMod.java
+++ java/src/WithJsonMod.java
@@ -1,22 +1,56 @@
+import arc.struct.Seq;
 import arc.util.Log;
-import mindustry.content.Liquids;
-import mindustry.content.StatusEffects;
-import mindustry.graphics.CacheLayer;
+import mindustry.content.*;
+import mindustry.graphics.Pal;
 import mindustry.mod.Mod;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.type.LiquidStack;
 import multicraft.*;
-public class WithJsonMod extends Mod {
-    public WithJsonMod(){
-        Log.info("WithJson Mod <init>");
-    }
-    @Override
-    public void init() {
-        Log.info("WithJson Mod init()");
-    }
-    @Override
-    public void loadContent() {
-        Log.info("WithJson Mod loadContent()");
-         new MultiCrafter("java-crafter"){{
 
-        }};
-    }
+public class WithJsonMod extends Mod {
+  public WithJsonMod() {
+    Log.info("WithJson Mod <init>");
+  }
+
+  @Override
+  public void init() {
+    Log.info("WithJson Mod init()");
+  }
+
+  @Override
+  public void loadContent() {
+    Log.info("WithJson Mod loadContent()");
+    new MultiCrafter("java-crafter") {{
+      requirements(Category.crafting, ItemStack.with(
+        Items.copper, 10
+      ));
+      selector = RecipeSelector.Detailed;
+      resolvedRecipes = Seq.with(
+        new Recipe() {{
+          input = new IOEntry() {{
+            items = Seq.with(new ItemStack(Items.copper, 1));
+          }};
+          output = new IOEntry() {{
+            items = Seq.with(new ItemStack(Items.copper, 1));
+          }};
+          craftTime = 160f;
+          icon = () -> Items.copper.uiIcon;
+          craftEffect = Fx.unitCapKill;
+          iconColor = Pal.accent;
+        }},
+        new Recipe() {{
+          input = new IOEntry() {{
+            fluids = Seq.with(new LiquidStack(Liquids.oil, 0.15f));
+          }};
+          output = new IOEntry() {{
+            items = Seq.with(new ItemStack(Items.coal, 2));
+            power = 2.5f;
+          }};
+          craftTime = 60f;
+          craftEffect = Fx.smoke;
+        }}
+      );
+    }};
+  }
 }
